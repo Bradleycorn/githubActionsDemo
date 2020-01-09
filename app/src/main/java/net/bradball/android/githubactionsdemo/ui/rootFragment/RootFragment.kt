7efe07102f2d61ac5fbfc060dc4eb2a7279352bb
brad.ball <brad.ball@twinspires.com>
2020-01-09 13:44:43 -0500

    Display version dynamically using value from gradle config

--- app/src/main/java/net/bradball/android/githubactionsdemo/ui/rootFragment/RootFragment.kt
+++ app/src/main/java/net/bradball/android/githubactionsdemo/ui/rootFragment/RootFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.viewModels
 import dagger.android.support.DaggerFragment
 
@@ -19,7 +20,11 @@ class RootFragment : DaggerFragment() {
     private val viewModel by viewModels<RootViewModel> { viewModelFactory }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_root, container, false)
+        val view = inflater.inflate(R.layout.fragment_root, container, false)
+
+        view.findViewById<TextView>(R.id.version_info).text = getString(R.string.app_version, viewModel.version)
+
+        return view
     }
 
 }
